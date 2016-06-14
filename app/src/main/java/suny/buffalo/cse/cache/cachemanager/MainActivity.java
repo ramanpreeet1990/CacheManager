07922b728e7b7f781918c512a8432f1284379a14
Ramanpreet Singh Khinda <ramanpreet1990@gmail.com>
2016-06-14 01:08:21 -0400

    Test 6

--- app/src/main/java/suny/buffalo/cse/cache/cachemanager/MainActivity.java
+++ app/src/main/java/suny/buffalo/cse/cache/cachemanager/MainActivity.java
@@ -25,7 +25,7 @@ import suny.buffalo.cse.cache.utility.Globals;
 import suny.buffalo.cse.cache.utility.Utility;
 
 /*
-* * Comment on my files : Testing
+* * Comment on my files : Testing a new
 * */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Session sessionObject;
